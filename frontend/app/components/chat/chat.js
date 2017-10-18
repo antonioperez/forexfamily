60340e1b4d525cc7ffab277799c6d188836cadd2
antonioperez <aperez2541@gmail.com>
2017-10-18 00:06:18 -0700

    inital working chat

--- frontend/app/components/chat/chat.js
+++ frontend/app/components/chat/chat.js
@@ -16,7 +16,6 @@
       
       self.currentUser = auth.currentUser;
       self.messages = {};
-      $scope.users = {};
       
       var setMessage = function(data) {
         $timeout(function(){
@@ -36,32 +35,6 @@
         this.messagesRef.limitToLast(loadLimit).on('child_changed', setMessage);
         this.messagesRef.limitToLast(loadLimit).on('child_removed', setMessage);
       }
-
-      self.saveMessage = function(messageInput) {
-        var currentUser = self.currentUser;
-        console.log(currentUser);
-        // Check that the user entered a message and is signed in.
-        if (messageInput && currentUser) {
-          // Add a new message entry to the Firebase Database.
-          //UTC
-          var milliseconds = Math.floor((new Date()).getTime() / 1000)
-          this.messagesRef = database.ref('messages');
-          this.messagesRef.push({
-            userid : currentUser.uid,
-            name: currentUser.displayName,
-            text: messageInput,
-            time: milliseconds,
-            photoUrl: currentUser.photoURL || '/components/layout/logo.png'
-
-          }).then(function() {
-            // Clear message text field and SEND button state.
-            // FriendlyChat.resetMaterialTextfield(this.messageInput);
-            // this.toggleButton();
-          }).catch(function(error) {
-            console.error('Error writing new message to Firebase Database', error);
-          });
-        }
-      };
 
       self.loadMessages();
 
---
+++ frontend/app/components/chat/message.js
@@ -0,0 +1,50 @@
+(function () {
+    
+      angular
+        .module('app')
+        .controller('ChatMessageCtrl', [
+          '$http',
+          '$scope', '$timeout',
+          Ctrl
+        ]);
+    
+        function Ctrl ($http, $scope, $timeout) {
+          var self = this;
+          var auth = firebase.auth();
+          var storageRef = firebase.storage().ref();
+          var database = firebase.database();
+          self.currentUser = auth.currentUser;
+
+          self.saveMessage = function() {
+            var currentUser = self.currentUser;
+            console.log(currentUser);
+            
+            var textfield = $('#summernote');
+            messageInput = textfield.summernote('code');
+            var plainText = $(messageInput).text();
+
+            console.log(plainText);
+            // Check that the user entered a message and is signed in.
+            if (messageInput && currentUser && plainText.length > 0) {
+              // Add a new message entry to the Firebase Database.
+              //UTC
+              var milliseconds = Math.floor((new Date()).getTime() / 1000)
+              this.messagesRef = database.ref('messages');
+              this.messagesRef.push({
+                userid : currentUser.uid,
+                name: currentUser.displayName,
+                text: messageInput,
+                time: milliseconds,
+                photoUrl: currentUser.photoURL || '/components/layout/logo.png'
+    
+              }).then(function() {
+                // Clear message text field and SEND button state.
+                textfield.summernote("reset");
+                // this.toggleButton();
+              }).catch(function(error) {
+                console.error('Error writing new message to Firebase Database', error);
+              });
+            }
+          };
+        }    
+    })();
